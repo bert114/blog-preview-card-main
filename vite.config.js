262f57ec966a88a77fb90c3aaf9ad1a7caf87a51
bert114 <janalbertpenano14@gmail.com>
2023-12-25 07:43:02 +0800

    updating the base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/results-summary-component-main/",
+  base: "/blog-preview-card-main/",
 });
