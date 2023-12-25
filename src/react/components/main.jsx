838833841859cdaaff12863a591f0995cb30b501
bert114 <janalbertpenano14@gmail.com>
2023-12-25 13:31:59 +0800

    cloning

--- src/react/components/main.jsx
+++ src/react/components/main.jsx
@@ -13,8 +13,8 @@ export default function Main() {
           <div>Published 21 Dec 2023</div>
           <h1>HTML & SCSS foundation</h1>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Debitis
-            saepe facilis temporibus consequuntur illo iure rem!
+            These languages are the backbone of every website, defining
+            structure, content and presentation
           </p>
           <div className="flex">
             <img src={profile} alt="user_profile" />
